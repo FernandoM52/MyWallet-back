be7839342dc62fbd1e826ea08024baa8555e07a7
FernandoM52 <fernandomartins.contato@outlook.com>
2023-04-22 16:12:59 -0300

    First commit: Estruturando projeto com express, mongodb

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import router from "./routes/index.routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router());
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
